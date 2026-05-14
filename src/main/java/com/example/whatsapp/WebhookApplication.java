d7da27670818d8a33eb5cb3327f8330ac53f8db0
Mayur SP <mayurshivaprem@gmail.com>
2026-05-15 03:31:13 +0530

    Enable scheduling in WebhookApplication

--- src/main/java/com/example/whatsapp/WebhookApplication.java
+++ src/main/java/com/example/whatsapp/WebhookApplication.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class WebhookApplication {
 
     public static void main(String[] args) {
